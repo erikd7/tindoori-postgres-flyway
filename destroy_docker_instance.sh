e3ab643cbfbc12be08fdc449bb29bc46f14197ae
erikd7 <eb.dietrich11@gmail.com>
2022-06-19 22:46:29 -0500

    Working!

--- destroy_docker_instance.sh
+++ destroy_docker_instance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-postgres.yml down -v
+docker compose -f docker-compose-postgres.yml down -v
--- tests_runner.sh
+++ tests_runner.sh
@@ -4,13 +4,13 @@ exec 2>/dev/null
 
 echo "Start PostgreSQL and Flyway in Docker Compose"
 
-docker-compose -f docker-compose-postgres.yml up > /dev/null &
+docker compose -f docker-compose-postgres.yml up > /dev/null &
 
 COUNT_FILES_TO_EXECUTE=$(ls -1 sql_versions | wc -l)
 docker run --rm --network="host" -v $(pwd)/tests:/tests pgtap-tester:latest -h 127.0.0.1 -p 5432 -u example-username -w pass -d db-name -t '/tests/*.sql' -f $COUNT_FILES_TO_EXECUTE  2>&1 | tee test_output.txt
 
 echo "Stop PostgreSQL and Flyway in Docker Compose"
-docker-compose -f docker-compose-postgres.yml down -v
+docker compose -f docker-compose-postgres.yml down -v
 
 TEST_RESULT=$(cat test_output.txt | grep Result: | cut -c9-100)
 
